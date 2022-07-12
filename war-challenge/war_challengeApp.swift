8ad5d170545b93de581f72393afde6a7a4b9fb86
Manuel Crovetto <87724520+ManuelCrovetto@users.noreply.github.com>
2022-07-12 23:14:56 +0200

    Initial Commit

---
+++ war-challenge/war_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_challengeApp.swift
+//  war-challenge
+//
+//  Created by Manuel Crovetto on 12/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct war_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
